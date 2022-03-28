8775fa7cd720ba7ae50ba872f4108597930a1c77
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-28 23:59:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+//
+// This program provides your address
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This program provides your address
+        int streetNumber;
+        string streetName;
+        Console.WriteLine("This program gets a user's street number and street name.");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter your street number.");
+        streetNumber =(Convert.ToInt32(Console.ReadLine()));
+        Console.Write("Enter your street name. ");
+        streetName = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine("Your info is: " + streetNumber + " "  + streetName + " ");
+
+        Console.WriteLine("\nDone.");
+    }
+}
